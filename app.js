289b994dea6ba391c1f6b50726c7ac08e38a1867
DMuir <david.muir@hymans.co.uk>
2024-09-16 17:05:18 +0100

    Expense form styling and functionality

--- app.js
+++ app.js
@@ -2,14 +2,129 @@ const incomeBtn = document.getElementById('incomeBtn');
 const outcomeBtn = document.getElementById('outcomeBtn');
 const referenceInput = document.getElementById('referenceId');
 const amountInput = document.getElementById('expenseAmount');
-const categorySelect = document.getElementById('categories');
+const categoryList = document.getElementById('categoriesContainer');
+// const categoryEntry = document.getElementById('category');
 const addBtn = document.getElementById('addBtn');
 
 let selectedType = null;
+let categoryType = null;
 let totalAmount = 0;
 let expenses = [];
 
-function toggleButton(clickedBtn, otherBtn) {
+const categoryObj = [
+    {
+        categoryName: 'Bills',
+        icon: 'fa-solid fa-lightbulb',
+        background: '#42A5F5'
+    },
+    {
+        categoryName: 'Charity',
+        icon: 'fa-solid fa-hand-holding-heart',
+        background: '#FF7043'
+    },
+    {
+        categoryName: 'Eating Out',
+        icon: 'fa-solid fa-utensils',
+        background: '#66BB6A'
+    },
+    {
+        categoryName: 'Entertainment',
+        icon: 'fa-solid fa-face-laugh',
+        background: '#EC407A'
+    },
+    {
+        categoryName: 'Expenses',
+        icon: 'fa-solid fa-receipt',
+        background: '#8D6E63'
+    },
+    {
+        categoryName: 'Family',
+        icon: 'fa-solid fa-house',
+        background: '#FFCA28'
+    },
+    {
+        categoryName: 'Finances',
+        icon: 'fa-solid fa-money-bill',
+        background: '#FFA726'
+    },
+    {
+        categoryName: 'General',
+        icon: 'fa-solid fa-shapes',
+        background: '#7E57C2'
+    },
+    {
+        categoryName: 'Gifts',
+        icon: 'fa-solid fa-gift',
+        background: '#26C6DA'
+    },
+    {
+        categoryName: 'Groceries',
+        icon: 'fa-solid fa-cart-shopping',
+        background: '#EF5350'
+    },
+    {
+        categoryName: 'Holidays',
+        icon: 'fa-solid fa-suitcase',
+        background: '#AB47BC'
+    },
+    {
+        categoryName: 'Income',
+        icon: 'fa-solid fa-arrow-left',
+        background: '#29B6F6'
+    },
+    {
+        categoryName: 'Personal',
+        icon: 'fa-solid fa-heart',
+        background: '#FF7043'
+    },
+    {
+        categoryName: 'Savings',
+        icon: 'fa-solid fa-seedling',
+        background: '#66BB6A'
+    },
+    {
+        categoryName: 'Shopping',
+        icon: 'fa-solid fa-bag-shopping',
+        background: '#5C6BC0'
+    },
+    {
+        categoryName: 'Transfers',
+        icon: 'fa-solid fa-money-bill-transfer',
+        background: '#FFCA28'
+    }
+];
+
+function categoryGenerator(obj) {
+    obj.forEach(category => {
+        let categoryDiv = document.createElement('div');
+        let categoryBtn = document.createElement('button');
+        let categoryTitle = document.createElement('p');
+        let categoryIcon = document.createElement('i');
+
+        categoryIcon.className = category.icon;
+        categoryTitle.textContent = category.categoryName;
+        categoryBtn.style.backgroundColor = category.background
+        categoryDiv.className = 'categoryDiv';
+
+        categoryBtn.addEventListener('click', () => {
+            const allButtons = document.querySelectorAll('.categoryDiv button');
+            allButtons.forEach(button => button.classList.remove('active'));
+
+            categoryBtn.classList.add('active');
+            categoryType = category.categoryName;
+        });
+
+        categoryBtn.appendChild(categoryIcon);
+        categoryDiv.appendChild(categoryBtn);
+        categoryDiv.appendChild(categoryTitle);
+
+        categoryList.appendChild(categoryDiv);
+    });
+}
+
+categoryGenerator(categoryObj);
+
+function toggleExpenseType(clickedBtn, otherBtn) {
     if (!clickedBtn.classList.contains('active')) {
         clickedBtn.classList.add('active');
         otherBtn.classList.remove('active');
@@ -18,19 +133,18 @@ function toggleButton(clickedBtn, otherBtn) {
 }
 
 incomeBtn.addEventListener('click', function () {
-    toggleButton(incomeBtn, outcomeBtn);
+    toggleExpenseType(incomeBtn, outcomeBtn);
 });
 
 outcomeBtn.addEventListener('click', function () {
-    toggleButton(outcomeBtn, incomeBtn);
+    toggleExpenseType(outcomeBtn, incomeBtn);
 });
 
-addBtn.addEventListener('click', function() {
+addBtn.addEventListener('click', function () {
     const reference = referenceInput.value;
     const amount = parseFloat(amountInput.value);
-    const category = categorySelect.value;
 
-    if(!selectedType || ! reference || isNaN(amount) || !category){
+    if (!selectedType || !reference || isNaN(amount) || !categoryType) {
         alert('Please fill out all fields and select Income/Outcome');
         return;
     }
@@ -39,20 +153,24 @@ addBtn.addEventListener('click', function() {
         type: selectedType,
         reference: reference,
         amount: amount,
-        category: category
+        category: categoryType
     };
 
     expenses.push(expense);
 
-    if(selectedType === 'Income'){
+    if (selectedType === 'Income') {
         totalAmount += amount;
-    } else if (selectedType === 'Outcome'){
+    } else if (selectedType === 'Outcome') {
         totalAmount -= amount;
     }
 
     referenceInput.value = '';
     amountInput.value = '';
-    categorySelect.value = '';
+
+    const allCategoryButtons = document.querySelectorAll('.categoryDiv button');
+    allCategoryButtons.forEach(button => {
+        button.classList.remove('active');
+    })
 
     incomeBtn.classList.remove('active');
     outcomeBtn.classList.remove('active');
